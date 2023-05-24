b930291626bd93cd3770ae29be0a62e35c817fbb
Leonardo Ruiz Bottura <leobottura12@gmail.com>
2023-05-24 19:08:50 -0300

    feat: Simple query with query methods

--- src/main/java/com/leonardoruiz/workshopmongo/dto/CommentDTO.java
+++ src/main/java/com/leonardoruiz/workshopmongo/dto/CommentDTO.java
@@ -3,7 +3,7 @@ package com.leonardoruiz.workshopmongo.dto;
 import java.io.Serializable;
 import java.util.Date;
 
-public class CommentDTO implements Serializable {
+public class CommentDTO implements Serializable  {
 
     private String text;
     private Date date;
